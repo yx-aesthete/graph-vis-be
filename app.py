a1e31a75be5a7404e50f1c59eee428e47474bff8
Michal-Madejski <m.madejski@perfectone.pl>
2024-07-04 21:23:14 +0200

    now

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from graph_algorithms import generate_traversal_path
 import os
 from graph_algorithms_v1 import (
     generate_bfs_sequence_v1,
@@ -16,15 +15,12 @@ from models import GraphDTO
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-REACT_APP_API_URL = os.environ.get(
-    "REACT_APP_API_URL", "http://127.0.0.1:5001")
+REACT_APP_API_URL = os.environ.get("REACT_APP_API_URL", "http://127.0.0.1:5001")
 port = int(os.environ.get("PORT", 5001))
-
 
 @app.route("/")
 def welcome():
     return "Welcome to GraphGen BE"
-
 
 @app.route("/traversal/bfs", methods=["POST"])
 def get_bfs_traversal():
@@ -37,7 +33,6 @@ def get_bfs_traversal():
         print(f"Error in get_bfs_traversal: {e}")
         return jsonify({"error": str(e)}), 500
 
-
 @app.route("/traversal/dfs", methods=["POST"])
 def get_dfs_traversal():
     try:
@@ -48,7 +43,6 @@ def get_dfs_traversal():
     except Exception as e:
         print(f"Error in get_dfs_traversal: {e}")
         return jsonify({"error": str(e)}), 500
-
 
 @app.route("/generate_random_tree_graph_for_traversal", methods=["POST"])
 def generate_random_tree_graph_endpoint():
@@ -62,7 +56,6 @@ def generate_random_tree_graph_endpoint():
         print(f"Error in generate_random_tree_graph: {e}")
         return jsonify({"error": str(e)}), 500
 
-
 @app.route("/generate_random_build_graph", methods=["POST"])
 def generate_random_graph_endpoint():
     try:
@@ -72,8 +65,7 @@ def generate_random_graph_endpoint():
         connectivity = data.get("connectivity", "random")
         additional_params = data.get("additional_params", {})
         print(f"Received request to generate graph with {num_nodes} nodes, {num_edges} edges, and {connectivity} connectivity")
-        graph = generate_random_build_graph(
-            num_nodes, num_edges, connectivity, **additional_params)
+        graph = generate_random_build_graph(num_nodes, num_edges, connectivity, **additional_params)
         graph_formats = get_graph_formats(graph)
         return jsonify({
             "graph": graph.dict(),
@@ -82,7 +74,6 @@ def generate_random_graph_endpoint():
     except Exception as e:
         print(f"Error in generate_random_graph: {e}")
         return jsonify({"error": str(e)}), 500
-
 
 @app.route("/traversal", methods=["POST"])
 def get_traversal():
@@ -97,13 +88,11 @@ def get_traversal():
         if not start_node or not algorithm:
             return jsonify({"error": "start_node and algorithm are required"}), 400
 
-        traversal_sequence = generate_traversal_path(
-            graph, start_node, algorithm, goal_node)
+        traversal_sequence = generate_traversal_path(graph, start_node, algorithm, goal_node)
         return jsonify(traversal_sequence)
     except Exception as e:
         print(f"Error in get_traversal: {e}")
         return jsonify({"error": str(e)}), 500
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
