47048d68919a250efeeae372492cd2fccc108e93
Michal-Madejski <m.madejski@perfectone.pl>
2024-07-05 00:37:25 +0200

    tryuing to hack graphviz

--- render-build.sh
+++ render-build.sh
@@ -3,7 +3,12 @@
 # Install system dependencies
 apt-get update && apt-get install -y graphviz graphviz-dev
 
+# Set environment variables for include and library paths
+export CPATH=/usr/include/graphviz/
+export LIBRARY_PATH=/usr/lib64/graphviz/
+
 # Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
 # Run the gunicorn server
